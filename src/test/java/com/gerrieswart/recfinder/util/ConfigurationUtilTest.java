380c86f9b6353a6b00ed2eecde798313c082d0c6
John Blackspear <johnblackspear@github.com>
2017-05-26 14:19:08 +0200

    setZoneBounds tests OK

--- src/test/java/com/gerrieswart/recfinder/util/ConfigurationUtilTest.java
+++ src/test/java/com/gerrieswart/recfinder/util/ConfigurationUtilTest.java
@@ -1,6 +1,8 @@
 package com.gerrieswart.recfinder.util;
 
 import com.gerrieswart.recfinder.Rover;
+import com.gerrieswart.recfinder.exception.InvalidZoneBoundsException;
+import com.gerrieswart.recfinder.exception.OutsideZoneBoundsException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -19,5 +21,24 @@ public class ConfigurationUtilTest
         assertEquals(rover.getExplorationZoneWidth(), 123);
         assertEquals(rover.getExplorationZoneHeight(), 456);
     }
+
+
+    @Test(expected = InvalidZoneBoundsException.class)
+    public void parserCatchesNegativeBounds() throws Exception
+    {
+        Rover             rover = new Rover();
+        ConfigurationUtil.setZoneBounds("    -10   40\t", rover);
+    }
+
+
+    @Test(expected = InvalidZoneBoundsException.class)
+    public void parserCatchesCompletelyMalformedBounds() throws Exception
+    {
+        Rover             rover = new Rover();
+        ConfigurationUtil.setZoneBounds("112a 2", rover);
+    }
+
+
+
 
 }
