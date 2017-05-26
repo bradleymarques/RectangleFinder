ca42a3fcbe451027d79487560dc39413483243ec
John Blackspear <johnblackspear@github.com>
2017-05-26 10:13:10 +0200

    Now ensures starting position can only be set once

--- src/main/java/com/gerrieswart/recfinder/Rover.java
+++ src/main/java/com/gerrieswart/recfinder/Rover.java
@@ -1,6 +1,7 @@
 package com.gerrieswart.recfinder;
 
 import com.gerrieswart.recfinder.exception.OutsideZoneBoundsException;
+import com.gerrieswart.recfinder.exception.ValueAlreadyModifiedException;
 
 /**
  * GJS
@@ -36,24 +37,45 @@ public class Rover
     }
 
 
-    public Rover setInitialXPosition(int x) throws OutsideZoneBoundsException
+    public Rover setStartingX(int x)
+            throws OutsideZoneBoundsException,
+                   ValueAlreadyModifiedException
+
     {
         if (x < 0)
         {
             throw new OutsideZoneBoundsException(
                     String.format("Rover's x position cannot be set to a negative value (%s)", x));
         }
-        this.x = x;
+
+        if (xIsDefault)
+        {
+            this.x = x;
+            xIsDefault = false;
+        } else
+        {
+            throw new ValueAlreadyModifiedException("X value already changed - can no longer set initial/starting position");
+        }
         return this;
     }
 
 
-    public Rover setInitialYPosition(int y) throws OutsideZoneBoundsException
+    public Rover setStartingY(int y)
+            throws OutsideZoneBoundsException,
+                   ValueAlreadyModifiedException
     {
         if (y < 0)
         {
             throw new OutsideZoneBoundsException(
                     String.format("Rover's y position cannot be set to a negative value (%s)", y));
+        }
+        if (yIsDefault)
+        {
+            this.y = y;
+            yIsDefault = false;
+        } else
+        {
+            throw new ValueAlreadyModifiedException("Y value already changed - can no longer set initial/starting position");
         }
         this.y = y;
         return this;
--- src/test/java/com/gerrieswart/recfinder/RoverPositionTest.java
+++ src/test/java/com/gerrieswart/recfinder/RoverPositionTest.java
@@ -1,6 +1,7 @@
 package com.gerrieswart.recfinder;
 
 import com.gerrieswart.recfinder.exception.OutsideZoneBoundsException;
+import com.gerrieswart.recfinder.exception.ValueAlreadyModifiedException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -24,7 +25,7 @@ public class RoverPositionTest
     @Test
     public void aNewRoverCanChangeItsXPos() throws Exception
     {
-        Rover r = new Rover().setInitialXPosition(1);
+        Rover r = new Rover().setStartingX(1);
 
         assertEquals(1, r.getX());
         assertEquals(0, r.getY());
@@ -34,7 +35,7 @@ public class RoverPositionTest
     @Test
     public void aNewRoverCanChangeItsYPos() throws Exception
     {
-        Rover r = new Rover().setInitialYPosition(1);
+        Rover r = new Rover().setStartingY(1);
 
         assertEquals(0, r.getX());
         assertEquals(1, r.getY());
@@ -44,7 +45,7 @@ public class RoverPositionTest
     @Test
     public void aNewRoverCanChangeItsInitialPos() throws Exception
     {
-        Rover r = new Rover().setInitialXPosition(1).setInitialYPosition(1);
+        Rover r = new Rover().setStartingX(1).setStartingY(1);
 
         assertEquals(1, r.getX());
         assertEquals(1, r.getY());
@@ -54,14 +55,31 @@ public class RoverPositionTest
     @Test(expected = OutsideZoneBoundsException.class)
     public void aRoverCannotGoWestOfOrigin() throws Exception
     {
-        new Rover().setInitialXPosition(-1);
+        new Rover().setStartingX(-1);
     }
 
 
     @Test(expected = OutsideZoneBoundsException.class)
     public void aRoverCannotGoSouthOfOrigin() throws Exception
     {
-        new Rover().setInitialYPosition(-1);
+        new Rover().setStartingY(-1);
     }
 
+
+    @Test(expected = ValueAlreadyModifiedException.class)
+    public void aRoverCannotChangeTheStartingXPositionMoreThanOnce() throws Exception
+    {
+        new Rover()
+                .setStartingX(1)
+                .setStartingX(1);
+    }
+
+
+    @Test(expected = ValueAlreadyModifiedException.class)
+    public void aRoverCannotChangeTheStartingYPositionMoreThanOnce() throws Exception
+    {
+        new Rover()
+                .setStartingY(1)
+                .setStartingY(1);
+    }
 }
