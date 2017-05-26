647be2f3fb8946e8fb8760b08f182a1ce3ffe002
John Blackspear <johnblackspear@github.com>
2017-05-26 09:46:05 +0200

    Parameterized the turn tests, added left turn testing too

---
+++ src/test/java/com/gerrieswart/recfinder/LeftTurnsTest.java
@@ -0,0 +1,55 @@
+package com.gerrieswart.recfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for the Rover's heading handling - right turns
+ */
+@RunWith(Parameterized.class)
+public class LeftTurnsTest
+{
+
+    int     amountOfTurns;
+    Heading expectedHeading;
+
+
+    public LeftTurnsTest(int amountOfTurns, Heading expectedHeading)
+    {
+        System.out.printf("Testing %s left turn(s), expecting: %s\n", amountOfTurns, expectedHeading);
+        this.amountOfTurns = amountOfTurns;
+        this.expectedHeading = expectedHeading;
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection leftTurns()
+    {
+        System.out.println("Testing left turns");
+        return asList(new Object[][]{
+                {1, Heading.WEST},
+                {2, Heading.SOUTH},
+                {3, Heading.EAST},
+                {4, Heading.NORTH},
+        });
+    }
+
+
+    @Test
+    public void testRightTurns()
+    {
+        Rover r = new Rover();
+        for (int i = 0; i < amountOfTurns; i++)
+        {
+            r.turnLeft();
+        }
+
+        assertEquals(expectedHeading, r.getHeading());
+    }
+}
---
+++ src/test/java/com/gerrieswart/recfinder/RightTurnsTest.java
@@ -0,0 +1,55 @@
+package com.gerrieswart.recfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for the Rover's heading handling - right turns
+ */
+@RunWith(Parameterized.class)
+public class RightTurnsTest
+{
+
+    int     amountOfTurns;
+    Heading expectedHeading;
+
+
+    public RightTurnsTest(int amountOfTurns, Heading expectedHeading)
+    {
+        System.out.printf("Testing %s right turn(s), expecting: %s\n", amountOfTurns, expectedHeading);
+        this.amountOfTurns = amountOfTurns;
+        this.expectedHeading = expectedHeading;
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection rightTurns()
+    {
+        System.out.println("Testing right turns");
+        return asList(new Object[][]{
+                {1, Heading.EAST},
+                {2, Heading.SOUTH},
+                {3, Heading.WEST},
+                {4, Heading.NORTH},
+        });
+    }
+
+
+    @Test
+    public void testRightTurns()
+    {
+        Rover r = new Rover();
+        for (int i = 0; i < amountOfTurns; i++)
+        {
+            r.turnRight();
+        }
+
+        assertEquals(expectedHeading, r.getHeading());
+    }
+}
